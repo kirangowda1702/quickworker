505e5beac2cc5df938ae102001d72c063470bdf5
Kiran Gowda <kirangowda143422@gmail.com>
2026-06-04 00:38:33 +0530

    Configure Capacitor and add Android platform files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kirangowda.quickworker',
+  appName: 'QuickWorker',
+  webDir: 'dist'
+};
+
+export default config;
